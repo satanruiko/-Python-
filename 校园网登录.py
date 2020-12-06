76547479e26392f3bf854457c79c50ad8843e627
satanruiko <lp932515@gmail.com>
2020-12-07 07:58:51 +0800

    无

---
+++ 校园网登录.py
@@ -0,0 +1,5 @@
+import requests
+data={'DDDDD':'028203200815@zndx','upass':'123456','R1':'0','R3':'0','R6':'0','para':'00','0MKKey':'123456'}
+r=requests.post(url='http://119.39.119.2/',data=data)
+
+
