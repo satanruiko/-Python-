78e0341df3b678303e3e5dc7599d659896bf8998
satanruiko <lp932515@gmail.com>
2020-12-27 23:53:39 +0800

    Create 整数拆分方法数计算.py

---
+++ 整数拆分方法数计算.py
@@ -0,0 +1,42 @@
+def odd():
+    n=1
+    while True:
+        n=n+2
+        yield n
+def number():
+    yield 1
+    n=1
+    while True:
+        n=n+1
+        yield n
+odds=odd();numbers=number()
+def gener():
+    while True:
+        yield next(numbers)
+        yield next(odds)
+geners=gener()
+def location():
+    n=1
+    yield n
+    while True:
+        n=n+next(geners)
+        yield n
+locations=location()
+def symbol(x):
+    if x%4==0:return 1
+    if x%4==1:return 1
+    if x%4==2:return -1
+    if x%4==3:return -1
+A=[1];B=[next(locations)];C=1
+while B[-1]>=len(A) and C<666:
+    a=0
+    for i in range(1,len(A)+1):
+        if i in B:
+            b=B.index(i)
+            a+=(A[-i]*symbol(b))
+    A.append(a)
+    C=len(A)
+    if B[-1]<=len(A):
+        B.append(next(locations))
+print(C)
+print(A[-1])
