13c3fb69572431df9584a474128c452ef693d6e8
satanruiko <lp932515@gmail.com>
2021-01-16 23:01:44 +0800

    Update sockboom_checkin.py

--- sockboom_checkin.py
+++ sockboom_checkin.py
@@ -1,10 +1,19 @@
 import requests
 import json
 
-sess=requests.Session()
-login_url='https://sockboom.art/auth/login'
-checkin_url='https://sockboom.art/user/checkin'
-data={'email':'lp932515@gmail.com','passwd':'12345678.'}
-req=sess.post(login_url,data=data)
-req=sess.post(checkin_url)
-print(req.json()['msg'])
+class checkin(object):
+    def __init__(self,email,passwd):
+        self.sess=requests.Session()
+        self.login_url='https://sockboom.art/auth/login'
+        self.checkin_url='https://sockboom.art/user/checkin'
+        self.email=email
+        self.passwd=passwd
+
+    def check(self):
+        data={'email':self.email,'passwd':self.passwd}
+        req=self.sess.post(self.login_url,data=data)
+        req=self.sess.post(self.checkin_url)
+        print(req.json()['msg'])
+
+eg=checkin('lp932515@gmail.com','12345678.')
+eg.check()
